2d5f12bbdba66f641ba3b441ebca8c0b0d4ca782
Manuel <manuel.baden@hotmail.de>
2022-06-14 21:58:38 +0200

    Updates

---
+++ precache-manifest.54c47c33cea759bd2e3f4c16cebe9b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1d4e7a32207fc88a6bf",
+    "url": "/place/static/css/main.d1172add.chunk.css"
+  },
+  {
+    "revision": "c1d4e7a32207fc88a6bf",
+    "url": "/place/static/js/main.c1d4e7a3.chunk.js"
+  },
+  {
+    "revision": "68ff129392958b2db0f8",
+    "url": "/place/static/js/1.68ff1293.chunk.js"
+  },
+  {
+    "revision": "dae8bd26d8bf810df8ef",
+    "url": "/place/static/js/2.dae8bd26.chunk.js"
+  },
+  {
+    "revision": "43e218245b268cdb24b7",
+    "url": "/place/static/js/runtime~main.43e21824.js"
+  },
+  {
+    "revision": "826382c2b5b12bcd154b3a971f1383a1",
+    "url": "/place/index.html"
+  }
+];
