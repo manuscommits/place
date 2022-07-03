d9690fd9feee2f3a89016081477fe99ef01bd980
Manuel <manuel.baden@hotmail.de>
2022-07-03 19:58:15 +0200

    Minor refactoring

--- Backend/database.js
+++ Backend/database.js
@@ -3,17 +3,6 @@ var sqlite3 = require("sqlite3").verbose();
 const DBSOURCE = "db.sqlite";
 
 const tableName = "pixels";
-const CREATE_TABLE = `CREATE TABLE ${tableName} (
-    x INTEGER,
-    y INTEGER,
-    color TEXT,
-    displayName TEXT NOT NULL, 
-    timestamp INTEGER NOT NULL,
-    PRIMARY KEY (x,y)
-    )`;
-const INSERT = `INSERT OR IGNORE INTO ${tableName} (x, y, color, displayName, timestamp) VALUES (?,?,?,?,?)`;
-const UPDATE_PIXEL = (x, y, color, displayName, timestmap) =>
-  `UPDATE ${tableName} SET color='${color}', displayName='${displayName}', timestamp='${timestmap}' WHERE x='${x}' AND y='${y}'`;
 
 const db = new sqlite3.Database(DBSOURCE, (err) => {
   if (err) {
@@ -21,20 +10,35 @@ const db = new sqlite3.Database(DBSOURCE, (err) => {
     throw err;
   }
   console.log("Connected to the SQLite database.");
-  db.run(CREATE_TABLE, (err) => {
-    if (err) {
-      // console.log("Table already exists.");
+  db.run(
+    `CREATE TABLE ${tableName} (
+    x INTEGER,
+    y INTEGER,
+    color TEXT,
+    displayName TEXT NOT NULL, 
+    timestamp INTEGER NOT NULL,
+    PRIMARY KEY (x,y)
+    )`,
+    (err) => {
+      if (err) {
+        // console.log("Table already exists.");
+      }
     }
-  });
+  );
 });
 
 const insert = (x, y, color, displayName) => {
   updatePixel(x, y, color, displayName);
-  db.run(INSERT, [x, y, color, displayName, Date.now()]);
+  db.run(
+    `INSERT OR IGNORE INTO ${tableName} (x, y, color, displayName, timestamp) VALUES (?,?,?,?,?)`,
+    [x, y, color, displayName, Date.now()]
+  );
 };
 
 const updatePixel = (x, y, color, displayName) => {
-  db.run(UPDATE_PIXEL(x, y, color, displayName, Date.now()));
+  db.run(
+    `UPDATE ${tableName} SET color='${color}', displayName='${displayName}', timestamp='${Date.now()}' WHERE x='${x}' AND y='${y}'`
+  );
 };
 
 const getAllPixels = (callback) => {
